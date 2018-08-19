faa5eed4d4d1b3c2150f156b57813ec6ebb5a7d8
Ryan Levick <ryan.levick@gmail.com>
2018-08-19 12:59:36 +0200

    Delete book's code folder

--- book/code/02-register_data_instructions/src/main.rs
+++
@@ -1,8 +0,0 @@
-#![allow(dead_code)]
-mod cpu;
-
-use cpu::CPU;
-
-fn main() {
-    CPU::new();
-}
--- book/code/03-program-counter/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod cpu;
-
-#[cfg(feature = "serialize")]
-extern crate serde;
-#[cfg(feature = "serialize")]
-#[macro_use]
-extern crate serde_derive;
