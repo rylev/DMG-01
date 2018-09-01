e754591797e4be73b1e722a02a6c873a55ce39ad
Ryan Levick <ryan.levick@gmail.com>
2018-09-01 16:25:12 +0200

    Rearrange project

--- dmg-01/src/main.rs
+++ dmg-01/src/main.rs
@@ -1,6 +1,6 @@
 extern crate minifb;
-extern crate image;
 extern crate clap;
+extern crate lib_dmg_01;
 
 use clap::{Arg, App};
 use minifb::{WindowOptions, Window, Key};
@@ -9,13 +9,12 @@ use std::io::Read;
 use std::time::{Instant, Duration};
 use std::thread::sleep;
 
-mod cpu;
-mod gpu;
-mod memory_bus;
+use lib_dmg_01::cpu::{ CPU };
 
 const ENLARGEMENT_FACTOR: usize = 1;
 const WINDOW_DIMENSIONS: [usize; 2] = [(160 * ENLARGEMENT_FACTOR),
                                      (144 * ENLARGEMENT_FACTOR)];
+
 fn main() {
     let matches = App::new("DMG-01")
         .author("Ryan Levick <ryan.levick@gmail.com>")
@@ -27,38 +26,39 @@ fn main() {
              .required(true)
              .value_name("FILE"))
         .get_matches();
-    let boot_buffer = matches.value_of("boot rom").map(|path| {
-        let mut boot = std::fs::File::open(path).expect("File not there");
-        let mut boot_buffer = Vec::new();
-        boot.read_to_end(&mut boot_buffer).expect("Could not read file");
-        boot_buffer
-    });
 
-    let rom_path = matches.value_of("rom").unwrap();
-    let mut game = std::fs::File::open(rom_path).expect("File not there");
-    let mut game_buffer = Vec::new();
-    game.read_to_end(&mut game_buffer).expect("Could not read file");
+    let boot_buffer = matches.value_of("boot rom").map(|path| buffer_from_file(path));
+    let game_buffer = matches.value_of("rom").map(|path| buffer_from_file(path)).unwrap();
 
-    let mut cpu = cpu::CPU::new(boot_buffer, game_buffer);
+    let cpu = CPU::new(boot_buffer, game_buffer);
+    let window = Window::new("DMG-01", WINDOW_DIMENSIONS[0], WINDOW_DIMENSIONS[1], WindowOptions::default()).unwrap();
 
-    let mut window = Window::new("DMG-01", WINDOW_DIMENSIONS[0], WINDOW_DIMENSIONS[1], WindowOptions::default()).unwrap();
+    run(cpu, window)
+}
 
-    let mut buffer = [0; 23040];
+const ONE_SECOND_IN_MICROS: usize = 1000000000;
+const ONE_SECOND_IN_CYCLES: usize = 4190000;
+const ONE_FRAME_IN_CYCLES: usize = 70224;
+const NUMBER_OF_PIXELS: usize = 23040;
+
+fn run(mut cpu: CPU, mut window: Window) {
+    let mut buffer = [0; NUMBER_OF_PIXELS];
     let mut cycles_elapsed_in_frame = 0usize;
     let mut now = Instant::now();
     while window.is_open() && !window.is_key_down(Key::Escape) {
         let time_delta = now.elapsed().subsec_nanos();
         now = Instant::now();
-        let delta = time_delta as f64 / 1000000000.0;
-        let cycles_to_run = (4190000.0 * delta) as usize;
+        let delta = time_delta as f64 / ONE_SECOND_IN_MICROS as f64;
+        let cycles_to_run = delta * ONE_SECOND_IN_CYCLES as f64;
 
         let mut cycles_elapsed = 0;
-        while cycles_elapsed <= cycles_to_run {
+        while cycles_elapsed <= cycles_to_run as usize{
             cycles_elapsed += cpu.step() as usize;
         }
         cycles_elapsed_in_frame += cycles_elapsed;
 
-        if cycles_elapsed_in_frame >= 70224 {
+        // TODO: Consider updating buffer after every line is rendered.
+        if cycles_elapsed_in_frame >= ONE_FRAME_IN_CYCLES {
             for (i, pixel) in cpu.bus.gpu.canvas_buffer.chunks(4).enumerate() {
                 buffer[i] = (pixel[3] as u32) << 24 | (pixel[2] as u32) << 16 | (pixel[1] as u32) << 8 | (pixel[0] as u32)
             }
@@ -68,4 +68,11 @@ fn main() {
             sleep(Duration::from_nanos(2))
         }
     }
+}
+
+fn buffer_from_file(path: &str) -> Vec<u8> {
+    let mut file = std::fs::File::open(path).expect("File not there");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Could not read file");
+    buffer
 }
--- lib-dmg-01-js/src/lib.rs
+++ lib-dmg-01-js/src/lib.rs
@@ -4,19 +4,21 @@
 extern crate serde_derive;
 
 extern crate wasm_bindgen;
-extern crate dmg_01;
+extern crate lib_dmg_01;
 
 use wasm_bindgen::prelude::*;
 
+use lib_dmg_01::cpu::instruction;
+
 #[wasm_bindgen]
 #[derive(Serialize)]
-pub struct CPU(dmg_01::cpu::CPU);
+pub struct CPU(lib_dmg_01::cpu::CPU);
 
 #[wasm_bindgen]
 impl CPU {
     #[wasm_bindgen(constructor)]
-    pub fn new() -> CPU {
-        let cpu = dmg_01::cpu::CPU::new();
+    pub fn new(boot_rom: Option<Vec<u8>>, game_rom: Vec<u8>) -> CPU {
+        let cpu = lib_dmg_01::cpu::CPU::new(boot_rom, game_rom);
         CPU(cpu)
     }
 
@@ -57,19 +59,19 @@ pub enum Register {
 pub fn add(cpu: CPU, target: Target) -> CPU {
     let mut cpu = cpu.0;
     let target = match target {
-        Target::A => Some(dmg_01::cpu::instruction::ArithmeticTarget::A),
-        Target::B => Some(dmg_01::cpu::instruction::ArithmeticTarget::B),
-        Target::C => Some(dmg_01::cpu::instruction::ArithmeticTarget::C),
-        Target::D => Some(dmg_01::cpu::instruction::ArithmeticTarget::D),
-        Target::E => Some(dmg_01::cpu::instruction::ArithmeticTarget::E),
-        Target::H => Some(dmg_01::cpu::instruction::ArithmeticTarget::H),
-        Target::L => Some(dmg_01::cpu::instruction::ArithmeticTarget::L),
+        Target::A => Some(instruction::ArithmeticTarget::A),
+        Target::B => Some(instruction::ArithmeticTarget::B),
+        Target::C => Some(instruction::ArithmeticTarget::C),
+        Target::D => Some(instruction::ArithmeticTarget::D),
+        Target::E => Some(instruction::ArithmeticTarget::E),
+        Target::H => Some(instruction::ArithmeticTarget::H),
+        Target::L => Some(instruction::ArithmeticTarget::L),
         // TODO: think about returning error for invalid targets
         _ => None
     };
 
     if let Some(target) = target {
-        cpu.execute(dmg_01::cpu::instruction::Instruction::ADD(target));
+        cpu.execute(instruction::Instruction::ADD(target));
     }
 
     CPU(cpu)
